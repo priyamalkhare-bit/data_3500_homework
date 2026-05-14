a9d6ff5daf6f171eb6e7c62a8fc14b8b1f90a078
Priya Malkhare USU <priya.malkhare@gmail.com>
2026-05-14 05:00:42 +0000

    Week 2 PA 3

---
+++ Programming_Activities/Week 2 - Programming Activity 3.py
@@ -0,0 +1,8 @@
+# read user input
+score = float(input("Enter your score percentage: "))
+
+# calculate answer
+if score >= 93:
+    print("Congratulations you got an A")
+else:
+    print("Congratulations, you still learned a ton!!!!")
